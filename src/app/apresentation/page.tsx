de83ca1b52735ab205ce7132166a5ab9d68253fa
Spiderlink-org <spiderlink.sites@gmail.com>
2024-04-18 20:15:54 -0300

    feat: Add ApresentationPage component and WavingHand SVG

--- src/app/apresentation/page.tsx
+++ src/app/apresentation/page.tsx
@@ -1,3 +1,24 @@
+import { WavingHand } from '@/shared/assets/svgs/WavingHand'
+
 export default function ApresentationPage() {
-  return <p>hello world</p>
+  return (
+    <main className="flex flex-col items-center justify-center py-[1rem]">
+      <div>
+        <div className="flex items-center justify-center">
+          <p className="font-bold text-[1.5rem] text-grey-50">Hello world</p>
+
+          <WavingHand className="w-[2.5rem]" />
+        </div>
+
+        <h1 className="font-extrabold text-[2.5rem] text-center leading-none text-grey-50">
+          Seja bem-vindo ao meu mundo digital!
+        </h1>
+
+        <p className="text-grey-200 font-medium text-[1rem] text-center mt-2">
+          Explore meu portfólio, descubra meus projetos e mergulhe no mundo da
+          programação comigo!
+        </p>
+      </div>
+    </main>
+  )
 }
