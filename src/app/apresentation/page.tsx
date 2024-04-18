b352f231d1d83ed91ed929da07edc6bab03ae091
Spiderlink-org <spiderlink.sites@gmail.com>
2024-04-18 14:54:50 -0300

    feat: Add ApresentationPage component and exit animation in InitialPage

---
+++ src/app/apresentation/page.tsx
@@ -0,0 +1,3 @@
+export default function ApresentationPage() {
+  return <p>hello world</p>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,72 @@
 'use client'
 
 import { LogoDXAnimation } from '@/shared/components/logo-dx-animation/Logo-DX-Animation'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { LoadingProgressBar } from './utils/components/loading-progress-bar/LoadingProgressBar'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function InitialPage() {
   const [dalay, setDelay] = useState(1000)
+  const [exit, setExit] = useState(false)
+  const router = useRouter()
 
   setTimeout(() => {
     setDelay(0)
   }, dalay)
 
+  useEffect(() => {
+    if (dalay === 0) {
+      setTimeout(() => {
+        setExit(true)
+      }, 1000)
+    }
+  }, [dalay])
+
+  useEffect(() => {
+    if (exit) {
+      setTimeout(() => {
+        router.push('/apresentation')
+      }, 600)
+    }
+  }, [exit, router])
+
   return (
-    <motion.main
-      className="flex flex-col justify-center items-center w-full h-full"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-    >
-      <motion.div className="flex flex-col justify-center items-center h-fit gap-5">
-        <LogoDXAnimation
-          initial={{ scale: 5 }}
-          animate={{ scale: 1 }}
-          transition={{ duration: 1, ease: 'easeInOut' }}
-        />
+    <AnimatePresence>
+      {!exit && (
+        <motion.main
+          className="flex flex-col justify-center items-center w-full h-full"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0, scale: 1.2 }}
+          transition={{ duration: 0.5, ease: 'easeInOut' }}
+        >
+          <motion.div className="flex flex-col justify-center items-center h-fit gap-5">
+            <LogoDXAnimation
+              initial={{ scale: 5 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 1, ease: 'easeInOut' }}
+            />
 
-        <LoadingProgressBar
-          style={{
-            opacity: dalay === 0 ? 1 : 0,
-            transition: 'opacity 2s',
-          }}
-        />
-      </motion.div>
+            <LoadingProgressBar
+              style={{
+                opacity: dalay === 0 ? 1 : 0,
+                transition: 'opacity 2s',
+              }}
+            />
+          </motion.div>
 
-      <p
-        className="absolute bottom-4"
-        style={{
-          opacity: dalay === 0 ? 1 : 0,
-          transition: 'opacity 1s',
-        }}
-      >
-        © Criado por Davi Alves - 2024
-      </p>
-    </motion.main>
+          <p
+            className="absolute bottom-4"
+            style={{
+              opacity: dalay === 0 ? 1 : 0,
+              transition: 'opacity 1s',
+            }}
+          >
+            © Criado por Davi Alves - 2024
+          </p>
+        </motion.main>
+      )}
+    </AnimatePresence>
   )
 }
