0612604d5f29823696f728e9e359ec7bbc0d88dc
daviaxs <cadwvk@gmail.com>
2024-04-20 15:15:10 -0300

    feat: Add Chat and Star SVG components

---
+++ src/shared/assets/svgs/Chat.tsx
@@ -0,0 +1,27 @@
+export function Chat(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={25}
+      height={26}
+      viewBox="0 0 25 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clipPath="url(#clip0_499_116)">
+        <path
+          d="M17.096 11.444h.01m-4.611 0h.01m-4.611 0h.01m.571 9.172H7.9c-4.6 0-6.9-1.146-6.9-6.879V8.004c0-4.586 2.3-6.879 6.9-6.879h9.2c4.6 0 6.9 2.293 6.9 6.88v5.732c0 4.586-2.3 6.88-6.9 6.88h-.575c-.356 0-.701.171-.92.458l-1.725 2.293c-.759 1.01-2.001 1.01-2.76 0l-1.725-2.293c-.184-.252-.61-.459-.92-.459z"
+          stroke="#fff"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_499_116">
+          <path fill="#fff" transform="translate(0 .125)" d="M0 0H25V25H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
