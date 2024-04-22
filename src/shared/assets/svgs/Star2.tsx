8f86aa21f23a69e9842cc5477e41a4f613e4b2f3
daviaxs <cadwvk@gmail.com>
2024-04-22 19:29:17 -0300

    setup: Add Star2 component to shared assets

---
+++ src/shared/assets/svgs/Star2.tsx
@@ -0,0 +1,75 @@
+export function Star2(props: React.SVGAttributes<SVGSVGElement>) {
+  return (
+    <svg
+      width={51}
+      height={40}
+      viewBox="0 0 51 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M19.71 40c-1.152 0-1.957-.391-2.44-.76-.92-.667-2.348-2.393-1.45-6.26l1.311-5.661c.116-.483-.138-1.358-.483-1.726l-4.58-4.58c-2.416-2.417-2.21-4.511-1.887-5.547.322-1.036 1.38-2.854 4.741-3.43l5.869-.966c.437-.069 1.15-.598 1.335-.99l3.245-6.49C26.936.46 29.007 0 30.112 0c1.105 0 3.176.46 4.741 3.59l3.245 6.49c.207.392.898.921 1.335.99l5.869.967c3.36.552 4.419 2.393 4.74 3.43.323 1.035.53 3.13-1.886 5.546l-4.58 4.58c-.346.345-.599 1.242-.484 1.726l1.312 5.662c.898 3.889-.53 5.592-1.45 6.26-.92.667-2.969 1.519-6.42-.53l-5.501-3.268c-.46-.276-1.428-.276-1.888 0l-5.5 3.268C22.057 39.678 20.722 40 19.709 40z"
+        fill="url(#paint0_linear_226_11)"
+      />
+      <path
+        d="M16.3 5.615H2.491A1.739 1.739 0 01.765 3.89c0-.943.783-1.726 1.726-1.726h13.81c.943 0 1.725.783 1.725 1.726 0 .944-.782 1.727-1.726 1.727z"
+        fill="url(#paint1_linear_226_11)"
+      />
+      <path
+        d="M9.396 37.836H2.49A1.739 1.739 0 01.765 36.11c0-.943.783-1.726 1.726-1.726h6.905c.943 0 1.726.783 1.726 1.726 0 .944-.783 1.726-1.726 1.726z"
+        fill="url(#paint2_linear_226_11)"
+      />
+      <path
+        d="M4.793 21.726H2.49A1.739 1.739 0 01.765 20c0-.944.783-1.726 1.726-1.726h2.302c.943 0 1.726.782 1.726 1.726 0 .943-.783 1.726-1.726 1.726z"
+        fill="url(#paint3_linear_226_11)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_226_11"
+          x1={46.0702}
+          y1={-8}
+          x2={-6.42981}
+          y2={50.5}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFB800" />
+          <stop offset={1} stopColor="#C65F00" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_226_11"
+          x1={46.0702}
+          y1={-8}
+          x2={-6.42981}
+          y2={50.5}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFB800" />
+          <stop offset={1} stopColor="#C65F00" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_226_11"
+          x1={46.0702}
+          y1={-8}
+          x2={-6.42981}
+          y2={50.5}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFB800" />
+          <stop offset={1} stopColor="#C65F00" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_226_11"
+          x1={46.0702}
+          y1={-8}
+          x2={-6.42981}
+          y2={50.5}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFB800" />
+          <stop offset={1} stopColor="#C65F00" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
