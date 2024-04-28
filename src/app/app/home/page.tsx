b4038c2cce0ca7ba3649d698a3c295ae6ffbff83
daviaxs <cadwvk@gmail.com>
2024-04-28 16:23:10 -0300

    feat: content add to home page

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -1,17 +1,39 @@
+import { WavingHand } from '@/shared/assets/svgs/WavingHand'
+import DaviAXSDesenhoViolet from '@/shared/assets/imgs/daviaxs-desenho-violet.png'
+import Image from 'next/image'
+
 export default function HomePage() {
   return (
     <div className="flex items-center justify-between w-full h-full">
-      <div className="flex flex-col items-start">
-        <p className="font-extrabold text-[38px]">Olá, eu sou</p>
+      <div className="flex flex-col items-start gap-2">
+        <div className="flex gap-1 ml-4 items-center justify-center bg-grey-500 rounded-md py-2 px-4 pointer-events-none">
+          <WavingHand className="h-[30px] w-[30px]" />
 
-        <h3 className="font-extrabold text-[76px] leading-[100%]">
-          Davi Alves
-        </h3>
+          <p className="text-[1.2rem] font-bold">Hello World</p>
+        </div>
 
-        <p className="font-extrabold text-[38px] text-green-200 decoration-green-200 decoration-2 underline">
-          Desenvolvedor Fullstack
-        </p>
+        <div className="flex items-center gap-2 h-[11rem] text-nowrap">
+          <span className="bg-gradient-to-t w-1 rounded-full from-green-200 to-[#fff] h-full" />
+
+          <div>
+            <p className="font-extrabold text-[2.375rem]">Olá, eu sou</p>
+
+            <h3 className="font-extrabold text-[4.75rem] leading-[100%]">
+              Davi Alves
+            </h3>
+
+            <p className="font-extrabold text-[2.375rem] text-green-200 decoration-green-200 decoration-2 underline">
+              Desenvolvedor Fullstack
+            </p>
+          </div>
+        </div>
       </div>
+
+      <Image
+        src={DaviAXSDesenhoViolet}
+        alt="desenho"
+        className="h-[75%] object-contain pointer-events-none"
+      />
     </div>
   )
 }
