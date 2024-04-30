44425cc716bb51fd846c7aa783f20c3455380862
daviaxs <cadwvk@gmail.com>
2024-04-29 22:15:06 -0300

    setup: Add Discord and Linkedin SVG components

---
+++ src/shared/assets/svgs/Linkedin.tsx
@@ -0,0 +1,17 @@
+export function Linkedin(props: React.SVGAttributes<SVGElement>) {
+  return (
+    <svg
+      width={30}
+      height={31}
+      viewBox="0 0 30 31"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M20 10.985a7.5 7.5 0 017.5 7.5v8.75h-5v-8.75a2.5 2.5 0 10-5 0v8.75h-5v-8.75a7.5 7.5 0 017.5-7.5zM7.5 12.235h-5v15h5v-15zM5 8.484a2.5 2.5 0 100-5 2.5 2.5 0 000 5z"
+        fill="#E9E9E9"
+      />
+    </svg>
+  )
+}
