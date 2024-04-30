3474dabd4a44a5766284334a02f79cfadf97c653
daviaxs <cadwvk@gmail.com>
2024-04-29 22:14:52 -0300

    css: Refactor ButtonSocialMedia component styles

--- src/shared/components/button-social-media/ButtonSocialMedia.tsx
+++ src/shared/components/button-social-media/ButtonSocialMedia.tsx
@@ -9,8 +9,8 @@ interface ButtonSocialMediaProps {
 export function ButtonSocialMedia({ children, href }: ButtonSocialMediaProps) {
   return (
     <Link href={href} target="_blank">
-      <button className="appearance-none cursor-pointer">
-        <div className="bg-grey-500 border-b-[2px] p-2 border-grey-400 rounded hover:bg-violet-400 hover:border-violet-500 transition-all">
+      <button className="appearance-none cursor-pointer w-[3rem] h-[2rem]">
+        <div className="flex items-center justify-center bg-grey-500 border-b-[2px] p-2 border-grey-400 rounded hover:bg-violet-400 hover:border-violet-500 transition-all">
           {children}
         </div>
       </button>
