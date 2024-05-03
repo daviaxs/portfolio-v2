fc0c0554915bf7d7382f38d89163f24687421b93
daviaxs <cadwvk@gmail.com>
2024-05-03 20:40:34 -0300

    refactor: Update home page layout for responsive design

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -8,25 +8,27 @@ import { Linkedin } from '@/shared/assets/svgs/Linkedin'
 
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-between w-full h-full">
+    <div className="flex items-center justify-between w-full h-full max-lg:flex-col max-lg:justify-start max-lg:items-center max-lg:gap-8 max-lg:pt-5">
       <div className="flex flex-col items-start gap-2">
-        <div className="flex gap-1 ml-4 items-center justify-center bg-grey-500 rounded-md py-2 px-4 pointer-events-none">
+        <div className="flex gap-1 ml-4 items-center justify-center bg-grey-500 rounded-md py-2 px-4 pointer-events-none max-lg:hidden">
           <WavingHand className="h-[30px] w-[30px]" />
 
           <p className="text-[1.2rem] font-bold">Hello World</p>
         </div>
 
         <div className="flex items-center gap-2 h-[11rem] text-nowrap">
-          <span className="bg-gradient-to-t w-1 rounded-full from-green-200 to-[#fff] h-full" />
+          <span className="bg-gradient-to-t w-1 rounded-full from-green-200 to-[#fff] h-full max-lg:hidden" />
 
-          <div>
-            <p className="font-extrabold text-[2.375rem]">Olá, eu sou</p>
+          <div className="max-lg:flex max-lg:flex-col max-lg:items-center">
+            <p className="font-extrabold text-[2.375rem] max-lg:text-[1.8rem] max-sm:text-[1.4rem]">
+              Olá, eu sou
+            </p>
 
-            <h3 className="font-extrabold text-[4.75rem] leading-[100%]">
+            <h3 className="font-extrabold text-[4.75rem] leading-[100%] max-lg:text-[4.2rem] max-sm:text-[3.3rem]">
               Davi Alves
             </h3>
 
-            <p className="font-extrabold text-[2.375rem] text-green-200 decoration-green-200 decoration-2 underline">
+            <p className="font-extrabold text-[2.375rem] text-green-200 decoration-green-200 decoration-2 underline max-lg:text-[1.8rem] max-sm:text-[1.4rem]">
               Desenvolvedor Fullstack
             </p>
           </div>
@@ -50,8 +52,10 @@ export default function HomePage() {
       <Image
         src={DaviAXSDesenhoViolet}
         alt="desenho"
-        className="h-[75%] object-contain pointer-events-none"
+        className="lg:h-[75%] object-contain pointer-events-none max-lg:h-fit w-[30rem]"
       />
+
+      <span className="lg:hidden">ㅤ</span>
     </div>
   )
 }
