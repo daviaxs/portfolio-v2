e7ae84f5248807a0fa3b5e2e80b290ea1df5355a
daviaxs <cadwvk@gmail.com>
2024-05-07 21:06:15 -0300

    feat: update CardAbout component

--- src/app/app/about/utils/components/card-about/CardAbout.tsx
+++ src/app/app/about/utils/components/card-about/CardAbout.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image'
+import DaviAxsDesenho from '@/shared/assets/imgs/daviaxs-desenho-com-bg.png'
+
 export function CardAbout() {
   return (
     <div className="flex flex-col w-full h-fit p-5 gap-3 rounded bg-grey-800 border border-grey-500">
-      <div>Sobre mim</div>
+      <div className="w-full h-fit flex gap-4 items-start">
+        <div className="relative w-[5.5rem] h-[5.5rem] rounded-full bg-gradient-to-r from-[#0085ff] via-[#a143ff] to-[#ff8383] z-0">
+          <Image
+            src={DaviAxsDesenho}
+            alt="avatar"
+            className="absolute top-[0.25rem] left-[0.25rem] w-[5rem] h-[5rem] rounded-full outline outline-[3px] outline-grey-800 z-10"
+          />
+        </div>
+
+        <div className="flex flex-col items-start gap-1">
+          <h4 className="font-bold text-[2.5rem] leading-[100%]">Sobre mim</h4>
+          <span className="h-[8px] w-[60%] bg-grey-500 rounded-full" />
+        </div>
+      </div>
 
       <div>
         Lorem Ipsus is simply dummy text of the printing and typesetting
