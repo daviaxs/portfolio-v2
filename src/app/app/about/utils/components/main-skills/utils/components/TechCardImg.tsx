e02adfaf185b3864a5714a3e2821b3ac9b66e9fc
daviaxs <cadwvk@gmail.com>
2024-05-12 18:28:04 -0300

    refactor: Update TechCardImg component to accept blur color prop

--- src/app/app/about/utils/components/main-skills/utils/components/TechCardImg.tsx
+++ src/app/app/about/utils/components/main-skills/utils/components/TechCardImg.tsx
@@ -1,10 +1,20 @@
-export function TechCardImg() {
+import { ReactNode } from 'react'
+
+interface TechCardImgProps {
+  blurColor: string
+  children: ReactNode
+}
+
+export function TechCardImg({ blurColor, children }: TechCardImgProps) {
   return (
     <div className="relative min-w-[4rem] min-h-[4rem]">
-      <div className="bg-blue-400 w-full h-full absolute blur-md" />
+      <div
+        className={`w-full h-full absolute blur-md`}
+        style={{ backgroundColor: blurColor, opacity: '0.7' }}
+      />
 
       <div className="absolute w-full h-full bg-grey-800 rounded-3xl flex items-center justify-center">
-        img
+        {children}
       </div>
     </div>
   )
