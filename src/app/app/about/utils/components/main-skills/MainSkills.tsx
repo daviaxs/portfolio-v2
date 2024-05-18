fd48e11c87bde6c15ce9674109959d3897a2805d
daviaxs <cadwvk@gmail.com>
2024-05-18 17:15:27 -0300

    feat: add Title component

--- src/app/app/about/utils/components/main-skills/MainSkills.tsx
+++ src/app/app/about/utils/components/main-skills/MainSkills.tsx
@@ -1,13 +1,12 @@
 import { TypeScript } from '@/shared/assets/svgs/TypeScript'
 import { TechCard } from './utils/components'
 import { ReactIcon } from '@/shared/assets/svgs/React'
+import { Title } from '../Title'
 
 export function MainSkills() {
   return (
     <div className="flex flex-col gap-6 items-start justify-start w-full h-fit">
-      <h2 className="font-extrabold text-[2rem] max-sm:text-[1.5rem] max-sm:text-center w-full">
-        Principais Habilidades
-      </h2>
+      <Title>Principais habilidades</Title>
 
       <div className="w-full flex items-center justify-between gap-4 max-md:flex-col">
         <TechCard.Root type="primary">
