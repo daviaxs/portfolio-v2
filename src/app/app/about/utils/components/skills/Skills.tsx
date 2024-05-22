eca272d9c9945c01e17e227b058bdd06649fefdc
daviaxs <cadwvk@gmail.com>
2024-05-21 21:05:39 -0300

    feat: create Skills component

---
+++ src/app/app/about/utils/components/skills/Skills.tsx
@@ -0,0 +1,9 @@
+import { Title } from '../Title'
+
+export function Skills() {
+  return (
+    <div className="flex flex-col gap-6 items-start justify-start w-full h-fit">
+      <Title>Minhas Habilidades</Title>
+    </div>
+  )
+}
