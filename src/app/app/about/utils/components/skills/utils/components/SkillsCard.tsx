ff0dab5e1393e5b5e73dd53f17c74761ee825f50
daviaxs <cadwvk@gmail.com>
2024-05-24 20:11:15 -0300

    feat: Add SkillsCard component

---
+++ src/app/app/about/utils/components/skills/utils/components/SkillsCard.tsx
@@ -0,0 +1,11 @@
+export function SkillsCard() {
+  return (
+    <div
+      className={`relative w-full h-[40rem] max-sm:h-[11rem] rounded from-[#0085ff] via-[#a143ff] via-36% to-[#ff8383] bg-gradient-to-br z-0`}
+    >
+      <div className="absolute top-[0.10rem] left-[0.10rem] right-[0.10rem] bottom-[0.10rem] bg-grey-800 rounded z-10 flex items-start justify-center gap-6 p-4">
+        Hello world
+      </div>
+    </div>
+  )
+}
