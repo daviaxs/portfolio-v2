316cf2289b4416ce41e2e170fbb0d0b46d6b4799
daviaxs <cadwvk@gmail.com>
2024-05-25 13:36:23 -0300

    feat: Update Skills component to use dynamic programming skills data

--- src/app/app/about/utils/components/skills/Skills.tsx
+++ src/app/app/about/utils/components/skills/Skills.tsx
@@ -1,7 +1,7 @@
 import { CodeXml } from 'lucide-react'
 import { Title } from '../Title'
 import { SkillsCard } from './utils/components'
-import { TypeScript } from '@/shared/assets/svgs/TypeScript'
+import { programmingSkills } from './programming-skills'
 
 export function Skills() {
   return (
@@ -20,10 +20,17 @@ export function Skills() {
           <SkillsCard.Separator />
 
           <SkillsCard.Techs.Techs>
-            <SkillsCard.Techs.Tech>
-              <TypeScript />
-              <SkillsCard.Techs.TechText>TypeScript</SkillsCard.Techs.TechText>
-            </SkillsCard.Techs.Tech>
+            {programmingSkills.map((skill, index) => (
+              <SkillsCard.Techs.Tech key={index}>
+                <div className="max-w-[3rem] min-w-[3rem] flex items-center justify-center">
+                  {skill.icon}
+                </div>
+
+                <SkillsCard.Techs.TechText>
+                  {skill.name}
+                </SkillsCard.Techs.TechText>
+              </SkillsCard.Techs.Tech>
+            ))}
           </SkillsCard.Techs.Techs>
         </SkillsCard.Root>
       </div>
---
+++ src/app/app/about/utils/components/skills/programming-skills.tsx
@@ -0,0 +1,68 @@
+import { CSS } from '@/shared/assets/svgs/CSS'
+import { Fastify } from '@/shared/assets/svgs/Fastify'
+import { FramerMotion } from '@/shared/assets/svgs/FramerMotion'
+import { Git } from '@/shared/assets/svgs/Git'
+import { JavaScript } from '@/shared/assets/svgs/JavaScript'
+import { NextJS } from '@/shared/assets/svgs/NextJS'
+import { Node } from '@/shared/assets/svgs/Node'
+import { Prisma } from '@/shared/assets/svgs/Prisma'
+import { ReactIcon } from '@/shared/assets/svgs/React'
+import { StyledComponents } from '@/shared/assets/svgs/StyledComponents'
+import { TailwindCSS } from '@/shared/assets/svgs/TailwindCSS'
+import { TypeScript } from '@/shared/assets/svgs/TypeScript'
+import { Zod } from '@/shared/assets/svgs/Zod'
+
+export const programmingSkills = [
+  {
+    icon: <TypeScript />,
+    name: 'TypeScript',
+  },
+  {
+    icon: <JavaScript />,
+    name: 'JavaScript',
+  },
+  {
+    icon: <Node />,
+    name: 'Node',
+  },
+  {
+    icon: <Prisma />,
+    name: 'Prisma',
+  },
+  {
+    icon: <Fastify />,
+    name: 'Fastify',
+  },
+  {
+    icon: <ReactIcon />,
+    name: 'ReactJS',
+  },
+  {
+    icon: <NextJS />,
+    name: 'Next.js',
+  },
+  {
+    icon: <FramerMotion />,
+    name: 'Framer Motion',
+  },
+  {
+    icon: <Zod />,
+    name: 'Zod',
+  },
+  {
+    icon: <CSS />,
+    name: 'CSS',
+  },
+  {
+    icon: <TailwindCSS />,
+    name: 'TaildwindCSS',
+  },
+  {
+    icon: <StyledComponents />,
+    name: 'Styled Components',
+  },
+  {
+    icon: <Git />,
+    name: 'Git',
+  },
+]
