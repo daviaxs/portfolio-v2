a136dea512c0b6f1061188ddfa8f94bb5d982caf
daviaxs <cadwvk@gmail.com>
2024-05-25 17:47:05 -0300

    feat: Add design skills to Skills component

--- src/app/app/about/utils/components/skills/Skills.tsx
+++ src/app/app/about/utils/components/skills/Skills.tsx
@@ -1,7 +1,8 @@
-import { CodeXml } from 'lucide-react'
+import { CodeXml, Palette } from 'lucide-react'
 import { Title } from '../Title'
 import { SkillsCard } from './utils/components'
 import { programmingSkills } from './programming-skills'
+import { designSkills } from './design-skills'
 
 export function Skills() {
   return (
@@ -21,6 +22,31 @@ export function Skills() {
 
           <SkillsCard.Techs.Techs>
             {programmingSkills.map((skill, index) => (
+              <SkillsCard.Techs.Tech key={index}>
+                <div className="max-w-[3rem] min-w-[3rem] flex items-center justify-center">
+                  {skill.icon}
+                </div>
+
+                <SkillsCard.Techs.TechText>
+                  {skill.name}
+                </SkillsCard.Techs.TechText>
+              </SkillsCard.Techs.Tech>
+            ))}
+          </SkillsCard.Techs.Techs>
+        </SkillsCard.Root>
+
+        <SkillsCard.Root>
+          <SkillsCard.Header.Header>
+            <Palette size={60} color="#55EE97" />
+            <SkillsCard.Header.HeaderTitle>
+              Design
+            </SkillsCard.Header.HeaderTitle>
+          </SkillsCard.Header.Header>
+
+          <SkillsCard.Separator />
+
+          <SkillsCard.Techs.Techs>
+            {designSkills.map((skill, index) => (
               <SkillsCard.Techs.Tech key={index}>
                 <div className="max-w-[3rem] min-w-[3rem] flex items-center justify-center">
                   {skill.icon}
---
+++ src/app/app/about/utils/components/skills/design-skills.tsx
@@ -0,0 +1,33 @@
+import { AfterEffects } from '@/shared/assets/svgs/AfterEffects'
+import { Canva } from '@/shared/assets/svgs/Canva'
+import { Figma } from '@/shared/assets/svgs/Figma'
+import { Gimp } from '@/shared/assets/svgs/Gimp'
+import { Illustrator } from '@/shared/assets/svgs/Illustrator'
+import { Photoshop } from '@/shared/assets/svgs/Photoshop'
+
+export const designSkills = [
+  {
+    icon: <Photoshop />,
+    name: 'Photoshop',
+  },
+  {
+    icon: <Illustrator />,
+    name: 'Illustrator',
+  },
+  {
+    icon: <AfterEffects />,
+    name: 'After Effects',
+  },
+  {
+    icon: <Gimp />,
+    name: 'Gimp',
+  },
+  {
+    icon: <Figma />,
+    name: 'Figma',
+  },
+  {
+    icon: <Canva />,
+    name: 'Canva',
+  },
+]
