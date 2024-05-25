e7d8d9ad04a1eefa92eb1d9e79781967e52fa606
daviaxs <cadwvk@gmail.com>
2024-05-25 17:46:53 -0300

    refactor: update bottom position to fix bug in NavigationMobile and NavigationTablet component

--- src/app/app/utils/components/navigation/NavigationMobile.tsx
+++ src/app/app/utils/components/navigation/NavigationMobile.tsx
@@ -30,7 +30,7 @@ export function NavigationMobile() {
   }
 
   return (
-    <menu className="h-fit w-full bg-grey-700 border-grey-500 flex items-center absolute justify-center rounded-tr-lg rounded-tl-lg bottom-0 px-4 py-2 z-20">
+    <menu className="h-fit w-full bg-grey-700 border-grey-500 flex items-center absolute justify-center rounded-tr-lg rounded-tl-lg bottom-[-1px] px-4 py-2 z-20">
       <nav className="flex items-end justify-between w-full gap-4 overflow-hidden">
         {mobileLinks.map((page) => (
           <Link
