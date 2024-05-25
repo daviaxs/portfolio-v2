6deca496c7d708775170c599c540336558f2a813
daviaxs <cadwvk@gmail.com>
2024-05-25 13:36:08 -0300

    css: Update SkillsCardTech height

--- src/app/app/about/utils/components/skills/utils/components/SkillsCardTech.tsx
+++ src/app/app/about/utils/components/skills/utils/components/SkillsCardTech.tsx
@@ -6,7 +6,7 @@ interface SkillsCardTechProps {
 
 export function SkillsCardTech({ children }: SkillsCardTechProps) {
   return (
-    <div className="bg-grey-600 w-full p-4 rounded-md flex items-center justify-start gap-4">
+    <div className="bg-grey-600 w-full min-h-[4rem] max-h-[4rem] p-4 rounded-md flex items-center justify-start gap-4">
       {children}
     </div>
   )
