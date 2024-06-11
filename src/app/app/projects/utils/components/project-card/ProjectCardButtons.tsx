bfda0e7960b8ffece36ec5bc2fa75ab7686031bb
daviaxs <cadwvk@gmail.com>
2024-06-11 19:54:06 -0300

    refactor: Update ProjectCardButtons component to support disabled state

--- src/app/app/projects/utils/components/project-card/ProjectCardButtons.tsx
+++ src/app/app/projects/utils/components/project-card/ProjectCardButtons.tsx
@@ -5,16 +5,25 @@ import Link from 'next/link'
 interface ProjectCardButtonsProps {
   deployUrl: string
   githubUrl: string
+
+  deployButtonDisabled?: boolean
+  githubButtonDisabled?: boolean
 }
 
 export function ProjectCardButtons({
   deployUrl,
   githubUrl,
+  deployButtonDisabled = false,
+  githubButtonDisabled = false,
 }: ProjectCardButtonsProps) {
   return (
     <div className="flex items-start justify-start gap-4">
       <Link href={deployUrl} target="_blank">
-        <Button.Root variant="small" color="violet">
+        <Button.Root
+          variant="small"
+          color="violet"
+          disabled={deployButtonDisabled}
+        >
           <Button.Text>Deploy</Button.Text>
           <Button.Icon>
             <Rocket />
@@ -23,7 +32,11 @@ export function ProjectCardButtons({
       </Link>
 
       <Link href={githubUrl} target="_blank">
-        <Button.Root variant="small" color="violet">
+        <Button.Root
+          variant="small"
+          color="violet"
+          disabled={githubButtonDisabled}
+        >
           <Button.Text>Github</Button.Text>
           <Button.Icon>
             <Github />
