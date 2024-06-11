118ad81b42e174f5b86ced8449d0a4c7280afc70
daviaxs <cadwvk@gmail.com>
2024-06-11 19:23:42 -0300

    feat: Add ProjectsList and ProjectsFeaturedList in ProjectsPage

--- src/app/app/projects/page.tsx
+++ src/app/app/projects/page.tsx
@@ -1,70 +1,69 @@
 import { Title } from '../about/utils/components/Title'
 import { ProjectCard } from './utils/components/project-card'
-import ImgNotLoad from './utils/components/img-not-load-base.png'
+import ImgNotLoad from '@/shared/assets/imgs/img-not-load-base.png'
+import { ProjectsFeaturedList } from './utils/ProjectsFeaturedList'
+import { ProjectsList } from './utils/ProjectsList'
 
 export default function ProjectsPage() {
   return (
     <div className="flex flex-col gap-6 py-4 items-start justify-start w-full h-fit">
       <Title>Projetos</Title>
 
-      <ProjectCard.RootFeatured>
-        <ProjectCard.Image src={ImgNotLoad} alt="" />
-
-        <ProjectCard.Content>
-          <ProjectCard.Title>SpiderLink</ProjectCard.Title>
-
-          <ProjectCard.Description>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
-            praesentium porro distinctio assumenda autem, iure natus at hic
-            iusto quas numquam ratione maxime! Temporibus accusamus beatae
-            ratione similique magnam numquam!
-          </ProjectCard.Description>
-
-          <ProjectCard.Techs>
-            <ProjectCard.Tech name="React" />
-            <ProjectCard.Tech name="Next.js" />
-            <ProjectCard.Tech name="TailwindCSS" />
-          </ProjectCard.Techs>
-
-          <ProjectCard.Buttons
-            deployUrl="https://spiderlink.vercel.app"
-            githubUrl=""
+      {ProjectsFeaturedList.map((project, index) => (
+        <ProjectCard.RootFeatured key={index}>
+          <ProjectCard.Image
+            src={project.img || ImgNotLoad}
+            alt={project.imgAlt}
           />
-        </ProjectCard.Content>
-      </ProjectCard.RootFeatured>
 
-      <ProjectCard.Root>
-        <ProjectCard.Image src={ImgNotLoad} alt="" />
+          <ProjectCard.Content>
+            <ProjectCard.Title>{project.name}</ProjectCard.Title>
 
-        <ProjectCard.Content>
-          <ProjectCard.Title>SpiderLink</ProjectCard.Title>
+            <ProjectCard.Description>
+              {project.description}
+            </ProjectCard.Description>
 
-          <ProjectCard.Description>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
-            praesentium porro distinctio assumenda autem, iure natus at hic
-            iusto quas numquam ratione maxime! Temporibus accusamus beatae
-            ratione similique magnam numquam! Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Assumenda fuga commodi dolore, sunt
-            consequatur eum velit accusamus illum vel iste deserunt porro iure
-            expedita eaque qui, quo, odio dolor facilis? Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Molestiae aspernatur porro,
-            obcaecati quasi modi soluta sint iusto? Aliquid voluptatibus
-            repellendus aut, dolor fugit voluptates, perspiciatis saepe dolorum
-            sequi nobis ipsa?
-          </ProjectCard.Description>
+            <ProjectCard.Techs>
+              {project.techs.map((tech, index) => (
+                <ProjectCard.Tech key={index} name={tech} />
+              ))}
+            </ProjectCard.Techs>
 
-          <ProjectCard.Techs>
-            <ProjectCard.Tech name="React" />
-            <ProjectCard.Tech name="Next.js" />
-            <ProjectCard.Tech name="TailwindCSS" />
-          </ProjectCard.Techs>
+            <ProjectCard.Buttons
+              deployUrl={project.deployUrl}
+              githubUrl={project.githubUrl}
+            />
+          </ProjectCard.Content>
+        </ProjectCard.RootFeatured>
+      ))}
 
-          <ProjectCard.Buttons
-            deployUrl="https://spiderlink.vercel.app"
-            githubUrl=""
+      {ProjectsList.map((project, index) => (
+        <ProjectCard.Root key={index}>
+          <ProjectCard.Image
+            src={project.img || ImgNotLoad}
+            alt={project.imgAlt}
           />
-        </ProjectCard.Content>
-      </ProjectCard.Root>
+
+          <ProjectCard.Content>
+            <ProjectCard.Title>{project.name}</ProjectCard.Title>
+
+            <ProjectCard.Description>
+              {project.description}
+            </ProjectCard.Description>
+
+            <ProjectCard.Techs>
+              {project.techs.map((tech, index) => (
+                <ProjectCard.Tech key={index} name={tech} />
+              ))}
+            </ProjectCard.Techs>
+
+            <ProjectCard.Buttons
+              deployUrl={project.deployUrl}
+              githubUrl={project.githubUrl}
+            />
+          </ProjectCard.Content>
+        </ProjectCard.Root>
+      ))}
     </div>
   )
 }
