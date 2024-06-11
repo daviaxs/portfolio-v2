979c08c1311930fbaadc316e1e329c37292f066d
daviaxs <cadwvk@gmail.com>
2024-06-11 19:23:11 -0300

    feat: Add ProjectsList utility file

---
+++ src/app/app/projects/utils/ProjectsList.ts
@@ -0,0 +1,12 @@
+export const ProjectsList = [
+  {
+    img: '',
+    imgAlt: '',
+    name: 'SpiderLink',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, praesentium porro distinctio assumenda autem, iure natus at hic iusto quas numquam ratione maxime! Temporibus accusamus beatae ratione similique magnam numquam!',
+    techs: ['React', 'Next.js', 'TailwindCSS'],
+    deployUrl: 'https://spiderlink.vercel.app',
+    githubUrl: '',
+  },
+]
