059df16f98a2c5683539cb0973a8b03336bbcbe7
daviaxs <cadwvk@gmail.com>
2024-06-15 13:07:21 -0300

    refactor: Update ProjectCardRootFeatured component to include text-nowrap css

--- src/app/app/projects/utils/components/project-card/ProjectCardRootFeatured.tsx
+++ src/app/app/projects/utils/components/project-card/ProjectCardRootFeatured.tsx
@@ -8,7 +8,7 @@ export function ProjectCardRootFeatured({ children }: ProjectCardTestProps) {
   return (
     <div className="relative w-full">
       <div className="absolute top-[-1rem] left-1/2 transform -translate-x-1/2 w-fit from-[#0085ff] via-[#a143ff] via-36% to-[#ff8383] bg-gradient-to-br z-20 px-2 py-1 rounded-xl">
-        <p className="font-bold">Projeto Destaque</p>
+        <p className="font-bold text-nowrap">Projeto Destaque</p>
       </div>
 
       <div className="flex items-start justify-start px-4 py-5 gap-4 w-full h-fit bg-grey-900 border-2 border-violet-300 rounded-lg max-md:flex-col max-md:items-center">
