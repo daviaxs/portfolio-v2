79b49c7cc01521dd0251a7985294428401588a01
daviaxs <cadwvk@gmail.com>
2024-06-15 13:10:38 -0300

    refactor: Update ProjectsFeaturedList techs array

--- src/app/app/projects/utils/ProjectsFeaturedList.ts
+++ src/app/app/projects/utils/ProjectsFeaturedList.ts
@@ -9,7 +9,18 @@ export const ProjectsFeaturedList = [
     description:
       "SpiderLink é uma empresa que vende sites completos e personalizados para diferentes nichos. Com um modelo pronto e adaptável às necessidades do cliente, oferece recursos como painel de admin, produtos, carrinho e mais. O nome Fritz's é um exemplo fictício, usado para demonstrar o design e funcionalidade do site.",
 
-    techs: ['React', 'Next.js', 'TailwindCSS'],
+    techs: [
+      'React JS',
+      'NextJS 14',
+      'TypeScript',
+      'Node',
+      'Styled Components',
+      'Radix UI',
+      'Fastify',
+      'Cloudinary',
+      'Prisma',
+      'Zod',
+    ],
 
     deployUrl: 'https://www.spider-link.pro/',
     githubUrl: '',
