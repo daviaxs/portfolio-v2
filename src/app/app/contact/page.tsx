2e37cfaf78949e32800771243fcf3aecf8e5e47e
daviaxs <cadwvk@gmail.com>
2024-06-24 19:54:34 -0300

    feat: Add form and input components to ContactPage

--- src/app/app/contact/page.tsx
+++ src/app/app/contact/page.tsx
@@ -1,9 +1,31 @@
+import { Mail, User } from 'lucide-react'
 import { Title } from '../utils/components/Title'
+import { InputContact } from './utils/components/input-contact'
 
 export default function ContactPage() {
   return (
-    <div className="flex flex-col items-center justify-start py-6 gap-5">
-      <Title className="text-center">Formulário de contato</Title>
+    <div className="flex flex-col items-center justify-start py-6 gap-5 px-20">
+      <div>
+        <Title className="text-center">Formulário de contato</Title>
+        <p className="font-bold text-grey-100 text-[1.2rem] text-center">
+          Se você tem um projeto interessante em mente ou precisa de ajuda em um
+          projeto já existente, entre em contato comigo agora mesmo!
+        </p>
+      </div>
+
+      <form className="flex flex-col w-full items-center justify-start gap-[1.5rem]">
+        <InputContact.Root>
+          <User size={30} className="text-grey-200" />
+
+          <InputContact.Input id="name" placeholder="Nome" type="text" />
+        </InputContact.Root>
+
+        <InputContact.Root>
+          <Mail size={30} className="text-grey-200" />
+
+          <InputContact.Input id="email" placeholder="Email" type="email" />
+        </InputContact.Root>
+      </form>
     </div>
   )
 }
