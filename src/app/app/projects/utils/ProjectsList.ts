a2313bc600c16d2c6d3e29ecb1d51243719e48e3
daviaxs <cadwvk@gmail.com>
2024-06-24 14:18:03 -0300

    feat: Add DX Store project in ProjectsList

--- src/app/app/projects/utils/ProjectsList.ts
+++ src/app/app/projects/utils/ProjectsList.ts
@@ -1,12 +1,21 @@
+import DxStoreImg from '@/shared/assets/imgs/dx-store.png'
+
 export const ProjectsList = [
   {
-    img: '',
-    imgAlt: '',
-    name: 'SpiderLink',
+    img: DxStoreImg,
+    imgAlt: 'dx-store',
+    name: 'DX Store',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, praesentium porro distinctio assumenda autem, iure natus at hic iusto quas numquam ratione maxime! Temporibus accusamus beatae ratione similique magnam numquam!',
-    techs: ['React', 'Next.js', 'TailwindCSS'],
-    deployUrl: 'https://spiderlink.vercel.app',
-    githubUrl: '',
+      'DX Store é um site de e-commerce desenvolvido com ReactJS e Prisma, oferecendo aos usuários a oportunidade de explorar e simular compras de produtos eletrônicos. Com a conveniência do login via Google, os visitantes podem experimentar o processo de compra, graças à integração do modo de teste do Stripe.',
+    techs: [
+      'React JS',
+      'Next-auth',
+      'TypeScript',
+      'Prisma',
+      'Tailwind CSS',
+      'Stripe',
+    ],
+    deployUrl: 'https://dx-store.vercel.app',
+    githubUrl: 'https://github.com/daviaxs/DX-Store',
   },
 ]
