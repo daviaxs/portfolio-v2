a9eb0122ac45229c1f43da9bcb0e16f013f5d479
daviaxs <cadwvk@gmail.com>
2024-06-24 14:40:23 -0300

    feat: Create ContactPage component

---
+++ src/app/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import { Title } from '../utils/components/Title'
+
+export default function ContactPage() {
+  return (
+    <div className="flex flex-col items-center justify-start py-6 gap-5">
+      <Title className="text-center">Formulário de contato</Title>
+    </div>
+  )
+}
