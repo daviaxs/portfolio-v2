b43b9bdb931e1804c52259e7d1a89deecf0e7b8a
daviaxs <cadwvk@gmail.com>
2024-06-28 13:53:12 -0300

    feat: Add FormSent page component

---
+++ src/app/form-sent/page.tsx
@@ -0,0 +1,11 @@
+import { Title } from '../utils/Title'
+
+export default function FormSent() {
+  return (
+    <main className="flex items-center justify-center w-full h-full">
+      <div>
+        <Title>Formulario enviado!</Title>
+      </div>
+    </main>
+  )
+}
