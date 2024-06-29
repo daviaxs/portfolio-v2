e3f0b513561cc0cf6d507d420dc551f7612358fe
daviaxs <cadwvk@gmail.com>
2024-06-29 20:09:34 -0300

    refactor: Update ButtonRoot component to accept a className prop

--- src/app/app/settings/utils/components/button-change-language/ButtonRoot.tsx
+++ src/app/app/settings/utils/components/button-change-language/ButtonRoot.tsx
@@ -2,12 +2,13 @@ import { HTMLAttributes, ReactNode } from 'react'
 
 interface ButtonRootProps extends HTMLAttributes<HTMLButtonElement> {
   children: ReactNode
+  className?: string
 }
 
-export function ButtonRoot({ children, ...props }: ButtonRootProps) {
+export function ButtonRoot({ className, children, ...props }: ButtonRootProps) {
   return (
     <button
-      className="flex items-center justify-between bg-grey-700 rounded-md p-4 cursor-pointer"
+      className={`flex items-center justify-between bg-grey-700 rounded-md p-4 cursor-pointer ${className}`}
       {...props}
     >
       {children}
