b672efaea4757ba8bbd3ff5a5f0c0f1d8d3de9b5
daviaxs <cadwvk@gmail.com>
2024-06-28 21:02:38 -0300

    feat: Add base language selection to Settings page

--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -1,3 +1,39 @@
+import { Title } from '@/app/utils/Title'
+import { Brazil } from '@/shared/assets/svgs/Brazil'
+import { UnitedStates } from '@/shared/assets/svgs/UnitedStates'
+import { Globe } from 'lucide-react'
+
 export default function Settings() {
-  return <div>hello world</div>
+  return (
+    <div className="flex flex-col gap-6 py-4 items-start justify-start w-full h-fit">
+      <Title>Configurações</Title>
+
+      <div className="flex flex-col justify-start items-start rounded-md w-full h-fit p-[1.2rem] gap-6 bg-grey-800">
+        <div className="flex items-center justify-center gap-4">
+          <Globe size={40} strokeWidth={1} />
+          <h2 className="font-bold text-[2rem]">Idioma</h2>
+        </div>
+
+        <div className="flex flex-col gap-4 w-full">
+          <button className="flex items-center justify-between bg-grey-700 rounded-md p-4 cursor-pointer">
+            <h4 className="font-bold text-[1.2rem]">Portugues</h4>
+
+            <div className="flex items-center justify-center gap-2">
+              <span className="font-bold text-grey-200">PT-BR</span>
+              <Brazil />
+            </div>
+          </button>
+
+          <button className="flex items-center justify-between bg-grey-700 rounded-md p-4 cursor-pointer">
+            <h4 className="font-bold text-[1.2rem]">Inglês</h4>
+
+            <div className="flex items-center justify-center gap-2">
+              <span className="font-bold text-grey-200">EN</span>
+              <UnitedStates />
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
