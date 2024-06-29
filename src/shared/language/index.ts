58431d23e319cb3e24f2e3589df74af33c43fac8
daviaxs <cadwvk@gmail.com>
2024-06-29 20:09:28 -0300

    feat: Add language options to LanguageContext

--- src/shared/language/index.ts
+++ src/shared/language/index.ts
@@ -1,3 +1,5 @@
+import { Brazil } from '../assets/svgs/Brazil'
+import { UnitedStates } from '../assets/svgs/UnitedStates'
 import { english } from './english'
 import { portugueseBrazil } from './portuguese-brazil'
 
@@ -8,3 +10,13 @@ export const language = {
 
 export type LanguageKeys = keyof typeof language
 export type Translation = (typeof language)[LanguageKeys]
+
+export const languageOptions: Array<{
+  key: LanguageKeys
+  title: string
+  icon: React.ComponentType
+  localeLabel: string
+}> = [
+  { key: 'PTBR', title: 'Português', icon: Brazil, localeLabel: 'PT-BR' },
+  { key: 'EN', title: 'Inglês', icon: UnitedStates, localeLabel: 'EN' },
+]
