a1d773f64edd00260bd170fa55a2e6fe7115391c
daviaxs <cadwvk@gmail.com>
2024-06-29 16:05:53 -0300

    feat: Update Settings page with language selection and translations

--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -1,37 +1,44 @@
+'use client'
+
 import { Title } from '@/app/utils/Title'
 import { Brazil } from '@/shared/assets/svgs/Brazil'
 import { UnitedStates } from '@/shared/assets/svgs/UnitedStates'
 import { Globe } from 'lucide-react'
+import { ButtonChangeLanguage } from './utils/components/button-change-language'
+import { useContext } from 'react'
+import { LanguageContext } from '@/shared/context/LanguageContext'
 
 export default function Settings() {
+  const { changeLanguage, translations } = useContext(LanguageContext)
+
   return (
     <div className="flex flex-col gap-6 py-4 items-start justify-start w-full h-fit">
-      <Title>Configurações</Title>
+      <Title>{translations.settingsPage.title}</Title>
 
       <div className="flex flex-col justify-start items-start rounded-md w-full h-fit p-[1.2rem] gap-6 bg-grey-800">
         <div className="flex items-center justify-center gap-4">
           <Globe size={40} strokeWidth={1} />
-          <h2 className="font-bold text-[2rem]">Idioma</h2>
+          <h2 className="font-bold text-[2rem]">
+            {translations.settingsPage.language.title}
+          </h2>
         </div>
 
         <div className="flex flex-col gap-4 w-full">
-          <button className="flex items-center justify-between bg-grey-700 rounded-md p-4 cursor-pointer">
-            <h4 className="font-bold text-[1.2rem]">Portugues</h4>
+          <ButtonChangeLanguage.Root onClick={() => changeLanguage('PTBR')}>
+            <ButtonChangeLanguage.Title>Português</ButtonChangeLanguage.Title>
 
-            <div className="flex items-center justify-center gap-2">
-              <span className="font-bold text-grey-200">PT-BR</span>
+            <ButtonChangeLanguage.LocaleIndicator label="PT-BR">
               <Brazil />
-            </div>
-          </button>
+            </ButtonChangeLanguage.LocaleIndicator>
+          </ButtonChangeLanguage.Root>
 
-          <button className="flex items-center justify-between bg-grey-700 rounded-md p-4 cursor-pointer">
-            <h4 className="font-bold text-[1.2rem]">Inglês</h4>
+          <ButtonChangeLanguage.Root onClick={() => changeLanguage('EN')}>
+            <ButtonChangeLanguage.Title>Inglês</ButtonChangeLanguage.Title>
 
-            <div className="flex items-center justify-center gap-2">
-              <span className="font-bold text-grey-200">EN</span>
+            <ButtonChangeLanguage.LocaleIndicator label="EN">
               <UnitedStates />
-            </div>
-          </button>
+            </ButtonChangeLanguage.LocaleIndicator>
+          </ButtonChangeLanguage.Root>
         </div>
       </div>
     </div>
