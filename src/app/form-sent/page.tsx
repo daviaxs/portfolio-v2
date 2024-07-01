81ccb69e0f0214447b09d7ee746ce86f5f7587b8
daviaxs <cadwvk@gmail.com>
2024-07-01 16:43:34 -0300

    refactor: Update FormSent page with translated title and description

--- src/app/form-sent/page.tsx
+++ src/app/form-sent/page.tsx
@@ -1,26 +1,33 @@
+'use client'
+
 import { Button } from '@/shared/components/button'
 import { Title } from '../utils/Title'
 import { Undo2 } from 'lucide-react'
 import Link from 'next/link'
 import { AnimatePage } from '../app/animatePage'
-import { env } from '@/env'
+import { LanguageContext } from '@/shared/context/LanguageContext'
+import { useContext } from 'react'
 
 export default function FormSent() {
+  const { translations } = useContext(LanguageContext)
+
   return (
     <AnimatePage>
       <main className="flex flex-col items-center justify-center gap-4 w-full h-full">
         <div>
-          <Title className="text-center">Formulario enviado!</Title>
+          <Title className="text-center">
+            {translations.formSentPage.title}
+          </Title>
 
           <p className="font-bold text-[1.2rem] text-center text-grey-100 max-sm:text-[1rem]">
-            Seu formulário foi entregue com sucesso! <br />
-            Aguarde minha resposta em breve.
+            {translations.formSentPage.description.part1} <br />
+            {translations.formSentPage.description.part2}
           </p>
         </div>
 
-        <Link href={`${env.WEB_SITE_URL}/app/home`}>
+        <Link href={`${process.env.WEB_SITE_URL}/app/home`}>
           <Button.Root>
-            <Button.Text>Voltar</Button.Text>
+            <Button.Text>{translations.formSentPage.buttonBack}</Button.Text>
             <Button.Icon>
               <Undo2 color="#fff" size={25} />
             </Button.Icon>
