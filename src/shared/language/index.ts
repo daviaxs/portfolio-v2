3e21439966feff9dd8380f46725d72977fd891ae
daviaxs <cadwvk@gmail.com>
2024-07-01 13:18:28 -0300

    refactor: Update language options in LanguageContext for better readability

--- src/shared/language/index.ts
+++ src/shared/language/index.ts
@@ -1,5 +1,3 @@
-import { Brazil } from '../assets/svgs/Brazil'
-import { UnitedStates } from '../assets/svgs/UnitedStates'
 import { english } from './english'
 import { portugueseBrazil } from './portuguese-brazil'
 
@@ -10,23 +8,3 @@ export const language = {
 
 export type LanguageKeys = keyof typeof language
 export type Translation = (typeof language)[LanguageKeys]
-
-export const languageOptions: Array<{
-  key: LanguageKeys
-  languageTitle: string
-  languageIcon: React.ComponentType
-  localeLabel: string
-}> = [
-  {
-    key: 'PTBR',
-    languageTitle: 'Português',
-    languageIcon: Brazil,
-    localeLabel: 'PT-BR',
-  },
-  {
-    key: 'EN',
-    languageTitle: 'Inglês',
-    languageIcon: UnitedStates,
-    localeLabel: 'EN',
-  },
-]
