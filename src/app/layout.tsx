f8728569d357e96a99ac8c34751344fb1fbab6a6
daviaxs <cadwvk@gmail.com>
2024-07-02 17:26:48 -0300

    feat: Add @vercel/analytics package for website analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import { LanguageProvider } from '@/shared/context/LanguageContext'
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 
 const inter = Nunito({
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br" className="scroll-smooth">
       <body className={inter.className}>
-        <LanguageProvider>{children}</LanguageProvider>
+        <LanguageProvider>
+          {children}
+          <Analytics />
+        </LanguageProvider>
       </body>
     </html>
   )
